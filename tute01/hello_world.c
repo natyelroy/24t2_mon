69ab6c33d7360f61da924dc3138816f07c49e101
natyelroy <natalie.leroyy@Gmail.com>
2024-05-27 18:08:19 +1000

    tute01 code

---
+++ tute01/hello_world.c
@@ -0,0 +1,13 @@
+// This is our first program :D
+// By class M17X
+// 27/05/2024
+
+#include <stdio.h>
+
+int main(void) {
+    printf("hello world\n");
+    printf(" /\\____/\\\n");
+    printf("(=' w ' =)\n");
+
+    return 0;
+}
